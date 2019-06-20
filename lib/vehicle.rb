e48dce9d27e39b918c8fa3dcfc94a4bc686b69f8
Silvia Mendoza <silviamendoza223@yahoo.com>
2019-06-20 01:48:24 +0000

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,24 @@
 class Vehicle
-
-end
+  
+  attr_accessor 
+  :wheel_size, :wheel_number
+  
+  def 
+    initialize(wheel_size, wheel_number)
+    
+    @wheel_size= wheel_size
+    
+    @wheel_number= wheel_number
+  end
+  
+  def go 
+    "vrrrrrroom!"
+  end
+  
+  def fill_up_tank
+    "filling up"!
+  end
+  
+  
+    
+    
